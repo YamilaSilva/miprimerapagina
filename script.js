18741458105652958088592b8c7aa08808e16b13
YamilaSilva <123336708+YamilaSilva@users.noreply.github.com>
2023-05-30 21:55:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// boton de alerta simple //
+function showAlert() {
+    alert('¡Hola, esta es una alerta desde JavaScript!');
+}
+
+// boton de alerta con getElementById & addEventListener //
+const miBoton = document.getElementById('miBoton')
+if (miBoton) {
+  miBoton.addEventListener('click', handleClick)
+}
+function handleClick(){
+    alert('¡Hola! Has hecho click en el botón.')
+}
+
+
+// formulario //
+const form = document.getElementById('myForm');
+if (form) {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    validateForm();
+  });
+}
+
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+  }
+function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+      alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+      alert('Correo electrónico enviado correctamente.');
+    }
+}
+
+// barra de navegación resposive //
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links")
+           .classList.toggle("nav-links-responsive")})
+
